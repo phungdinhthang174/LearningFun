8acac0be08f587e7ec958e3372feb2481daffeef
LENOVO <phungdinhthang174@gmail.com>
2021-11-23 08:38:45 +0700

    lf

---
+++ app/src/main/java/com/example/learningfun/WelcomeScreen.java
@@ -0,0 +1,24 @@
+package com.example.learningfun;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class WelcomeScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome_screen);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(WelcomeScreen.this,SignInActivity.class));
+                finish();
+            }
+        },4000);
+
+    }
+}
